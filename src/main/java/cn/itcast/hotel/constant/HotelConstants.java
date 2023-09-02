9a999fd3b612440f69243d9f805a4f5feff9af98
wangchuan <1016368304@qq.com>
2023-09-02 23:59:07 +0800

    创建索引库

---
+++ src/main/java/cn/itcast/hotel/constant/HotelConstants.java
@@ -0,0 +1,54 @@
+package cn.itcast.hotel.constant;
+
+
+public class HotelConstants {
+    public static final String MAPPING_TEMPLATE = "{\n" +
+            "  \"mappings\": {\n" +
+            "    \"properties\": {\n" +
+            "      \"id\": {\n" +
+            "        \"type\": \"keyword\"\n" +
+            "      },\n" +
+            "      \"name\":{\n" +
+            "        \"type\": \"text\",\n" +
+            "        \"analyzer\": \"ik_max_word\",\n" +
+            "        \"copy_to\": \"{all}\"\n" +
+            "      },\n" +
+            "      \"address\":{\n" +
+            "        \"type\": \"keyword\",\n" +
+            "        \"index\": false\n" +
+            "      },\n" +
+            "      \"price\":{\n" +
+            "        \"type\": \"integer\"\n" +
+            "      },\n" +
+            "      \"score\":{\n" +
+            "        \"type\": \"integer\"\n" +
+            "      },\n" +
+            "      \"brand\":{\n" +
+            "        \"type\": \"binary\",\n" +
+            "        \"copy_to\": \"{all}\"\n" +
+            "      },\n" +
+            "      \"city\":{\n" +
+            "        \"type\": \"integer\"\n" +
+            "      },\n" +
+            "      \"startName\":{\n" +
+            "        \"type\": \"integer\"\n" +
+            "      },\n" +
+            "      \"business\":{\n" +
+            "        \"type\": \"integer\",\n" +
+            "        \"copy_to\": \"{all}\"\n" +
+            "      },\n" +
+            "      \"location\":{\n" +
+            "        \"type\": \"geo_point\"\n" +
+            "      },\n" +
+            "      \"pic\":{\n" +
+            "        \"type\": \"keyword\",\n" +
+            "        \"index\": false\n" +
+            "      },\n" +
+            "      \"all\":{\n" +
+            "        \"type\": \"text\",\n" +
+            "        \"analyzer\": \"ik_max_word\"\n" +
+            "      }\n" +
+            "    }\n" +
+            "  }\n" +
+            "}";
+}
