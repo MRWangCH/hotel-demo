00ba137b9f795e493cd6b26c941565ef6c4086d1
wangchuan <1016368304@qq.com>
2023-09-03 11:09:08 +0800

    删除索引库以及判断索引库是否存在

--- src/test/java/cn/itcast/hotel/HotelIndexTest.java
+++ src/test/java/cn/itcast/hotel/HotelIndexTest.java
@@ -1,10 +1,12 @@
 package cn.itcast.hotel;
 
 import org.apache.http.HttpHost;
+import org.elasticsearch.action.admin.indices.delete.DeleteIndexRequest;
 import org.elasticsearch.client.RequestOptions;
 import org.elasticsearch.client.RestClient;
 import org.elasticsearch.client.RestHighLevelClient;
 import org.elasticsearch.client.indices.CreateIndexRequest;
+import org.elasticsearch.client.indices.GetIndexRequest;
 import org.elasticsearch.common.xcontent.XContentType;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
@@ -18,7 +20,7 @@ public class HotelIndexTest {
     private RestHighLevelClient client;
 
     @Test
-    void testInit(){
+    void testInit() {
         System.out.println(client);
     }
 
@@ -30,6 +32,24 @@ public class HotelIndexTest {
         request.source(MAPPING_TEMPLATE, XContentType.JSON);
         //3.发送请求
         client.indices().create(request, RequestOptions.DEFAULT);
+    }
+
+    @Test
+    void testDeleteHotelIndex() throws IOException {
+        //1.创建request对象
+        DeleteIndexRequest request = new DeleteIndexRequest("hotel");
+        //2.发送请求
+        client.indices().delete(request, RequestOptions.DEFAULT);
+    }
+
+    @Test
+    void testExistsHotelIndex() throws IOException {
+        //1.创建request对象
+        GetIndexRequest request = new GetIndexRequest("hotel");
+        //2.发送请求
+        boolean exists = client.indices().exists(request, RequestOptions.DEFAULT);
+        //3.输出
+        System.out.println(exists ? "索引库存在" : "索引库不存在");
     }
 
     @BeforeEach
