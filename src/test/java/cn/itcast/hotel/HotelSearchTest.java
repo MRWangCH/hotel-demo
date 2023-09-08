50f5b29d479bd4d45ff60c0523a2d6799eef6218
wangchuan <1016368304@qq.com>
2023-09-08 23:56:41 +0800

    QueryBuilders的term查询与range查询

--- src/test/java/cn/itcast/hotel/HotelSearchTest.java
+++ src/test/java/cn/itcast/hotel/HotelSearchTest.java
@@ -3,26 +3,20 @@ package cn.itcast.hotel;
 import cn.itcast.hotel.pojo.HotelDoc;
 import com.alibaba.fastjson.JSON;
 import org.apache.http.HttpHost;
-import org.elasticsearch.action.admin.indices.delete.DeleteIndexRequest;
 import org.elasticsearch.action.search.SearchRequest;
 import org.elasticsearch.action.search.SearchResponse;
 import org.elasticsearch.client.RequestOptions;
 import org.elasticsearch.client.RestClient;
 import org.elasticsearch.client.RestHighLevelClient;
-import org.elasticsearch.client.indices.CreateIndexRequest;
-import org.elasticsearch.client.indices.GetIndexRequest;
-import org.elasticsearch.common.xcontent.XContentType;
+import org.elasticsearch.index.query.BoolQueryBuilder;
 import org.elasticsearch.index.query.QueryBuilders;
 import org.elasticsearch.search.SearchHit;
 import org.elasticsearch.search.SearchHits;
-import org.elasticsearch.search.builder.SearchSourceBuilder;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
-
-import static cn.itcast.hotel.constant.HotelConstants.MAPPING_TEMPLATE;
 
 public class HotelSearchTest {
     private RestHighLevelClient client;
@@ -35,6 +29,40 @@ public class HotelSearchTest {
         request.source().query(QueryBuilders.matchAllQuery());
         //3.发送请求
         SearchResponse response = client.search(request, RequestOptions.DEFAULT);
+        //4.解析结果
+        handleResponse(response);
+    }
+
+
+    @Test
+    void testMatch() throws IOException {
+        //1.准备request
+        SearchRequest request = new SearchRequest("hotel");
+        //2.准备DSL
+        request.source().query(QueryBuilders.matchQuery("all","如家"));
+        //3.发送请求
+        SearchResponse response = client.search(request, RequestOptions.DEFAULT);
+        handleResponse(response);
+    }
+
+
+    @Test
+    void testBool() throws IOException {
+        //1.准备request
+        SearchRequest request = new SearchRequest("hotel");
+        //2.准备DSL
+        //2.1准备booleanQuery
+        BoolQueryBuilder boolQuery = QueryBuilders.boolQuery();
+        //2.2准备term
+        boolQuery.must(QueryBuilders.termQuery("city", "杭州"));
+        //2.3准备range
+        boolQuery.filter(QueryBuilders.rangeQuery("price").lte(250));
+        request.source().query(boolQuery);
+        //3.发送请求
+        SearchResponse response = client.search(request, RequestOptions.DEFAULT);
+        handleResponse(response);
+    }
+    private void handleResponse(SearchResponse response) {
         //4.解析结果
         SearchHits searchHits = response.getHits();
         //4.1获取总条数
