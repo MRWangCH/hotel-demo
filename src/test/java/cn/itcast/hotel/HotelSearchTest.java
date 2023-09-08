21f6c0844a571453f80140ec5327a49aa4e12b0f
wangchuan <1016368304@qq.com>
2023-09-09 00:07:44 +0800

    排序和分页

--- src/test/java/cn/itcast/hotel/HotelSearchTest.java
+++ src/test/java/cn/itcast/hotel/HotelSearchTest.java
@@ -12,6 +12,7 @@ import org.elasticsearch.index.query.BoolQueryBuilder;
 import org.elasticsearch.index.query.QueryBuilders;
 import org.elasticsearch.search.SearchHit;
 import org.elasticsearch.search.SearchHits;
+import org.elasticsearch.search.sort.SortOrder;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -62,6 +63,27 @@ public class HotelSearchTest {
         SearchResponse response = client.search(request, RequestOptions.DEFAULT);
         handleResponse(response);
     }
+
+
+    @Test
+    void testPageAndSort() throws IOException {
+        //页码，每页大小
+        int page = 2, size = 5;
+        //1.准备request
+        SearchRequest request = new SearchRequest("hotel");
+        //2.准备DSL
+        //2.1准备query
+        request.source().query(QueryBuilders.matchAllQuery());
+        //2.2准备sort
+        request.source().sort("price", SortOrder.ASC);
+        //2.3分页form、size
+        request.source().from((page - 1) * size).size(5);
+        //3.发送请求
+        SearchResponse response = client.search(request, RequestOptions.DEFAULT);
+        handleResponse(response);
+    }
+
+
     private void handleResponse(SearchResponse response) {
         //4.解析结果
         SearchHits searchHits = response.getHits();
